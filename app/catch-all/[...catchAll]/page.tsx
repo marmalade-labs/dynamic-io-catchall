96fc9cae72c543268d23953bcdd591275f803ebf
Steve McClain <steve@marmalade.co>
2024-11-27 17:04:02 -0500

    Enable PPR + dynamicIO and add catchAll

---
+++ app/catch-all/[...catchAll]/page.tsx
@@ -0,0 +1,5 @@
+export default async function Page({ params }: { params: Promise<{ catchAll: string[] }> }) {
+  const { catchAll } = await params
+  console.log({ catchAll });
+  return <div>{catchAll}</div>
+}
