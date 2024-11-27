0b2549d1ecf905da0cc6a2b249a0f00abb5ae62e
Steve McClain <steve@marmalade.co>
2024-11-27 17:20:30 -0500

    Await but return null

--- app/catch-all/[...catchAll]/page.tsx
+++ app/catch-all/[...catchAll]/page.tsx
@@ -1,3 +1,5 @@
-export default async function Page() {
+export default async function Page({ params }: { params: Promise<{ catchAll: string[] }> }) {
+  const { catchAll } = await params
+  console.log({ catchAll });
   return null;
 }
