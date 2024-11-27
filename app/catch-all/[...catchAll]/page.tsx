700ccb852cb91ca974799df31e09085bcc8bffb8
Steve McClain <steve@marmalade.co>
2024-11-27 17:24:45 -0500

    Re-enable

--- app/catch-all/[...catchAll]/page.tsx
+++ app/catch-all/[...catchAll]/page.tsx
@@ -1,5 +1,7 @@
 export default async function Page({ params }: { params: Promise<{ catchAll: string[] }> }) {
-  // const { catchAll } = await params
-  // console.log({ catchAll });
+
+  const { catchAll } = await params
+  console.log({ catchAll });
+
   return null;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     ppr: true,
-    // dynamicIO: true,
+    dynamicIO: true,
   },
 };
 
