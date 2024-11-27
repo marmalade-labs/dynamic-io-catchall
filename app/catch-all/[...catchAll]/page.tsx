29d4335885301aa044b53a2ca21e74a9061f4e23
Steve McClain <steve@marmalade.co>
2024-11-27 17:26:34 -0500

    Add docs

--- app/catch-all/[...catchAll]/page.tsx
+++ app/catch-all/[...catchAll]/page.tsx
@@ -1,5 +1,10 @@
 export default async function Page({ params }: { params: Promise<{ catchAll: string[] }> }) {
 
+  // Comment out these two lines and the Vercel build will fail:
+  //   Traced Next.js server files in: 113.83ms
+  //   Created all serverless functions in: 108.445ms
+  //   Collected static files (public/, static/, .next/static): 3.331ms
+  //   Error: EISDIR: illegal operation on a directory, read
   const { catchAll } = await params
   console.log({ catchAll });
 
