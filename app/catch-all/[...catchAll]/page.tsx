1e879ae2e6cfc5356f821f54118d600d82f151c4
Steve McClain <steve@marmalade.co>
2024-11-27 17:18:51 -0500

    Async but empty

--- app/catch-all/[...catchAll]/page.tsx
+++ app/catch-all/[...catchAll]/page.tsx
@@ -1,5 +1,3 @@
-export default async function Page({ params }: { params: Promise<{ catchAll: string[] }> }) {
-  const { catchAll } = await params
-  console.log({ catchAll });
-  return <div>{catchAll}</div>
+export default async function Page() {
+  return null;
 }
